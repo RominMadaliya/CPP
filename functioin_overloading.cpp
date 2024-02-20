a407a0d87a72fb1dc2699dec039c968c3440dc06
RominMadaliya <rominskillqode@gmail.com>
2024-02-20 10:01:47 +0530

    20/04/2024

--- functioin_overloading.cpp
+++ functioin_overloading.cpp
@@ -1,44 +1,61 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 void convert(double dol)
-{ 
+{
     int rupees;
-    rupees=dol*82;
-    cout<<"The rupees is :"<<rupees<<endl;
+    rupees = dol * 82;
+    cout << "The rupees is :" << rupees << endl;
 }
 float convert(float feet)
 {
 
-    float inch=feet*12;
+    float inch = feet * 12;
     return inch;
 }
 
 void convert(int inch)
 {
-    int centimeter = (float)inch*2.54;
-    cout<<"The centimeter is :"<<centimeter<<endl;
+    int centimeter = (float)inch * 2.54;
+    cout << "The centimeter is :" << centimeter << endl;
+}
+
+struct rom
+{
+    float celsius;
+    
+    };
+
+rom to_farenheit(rom an)
+{
+    rom ans;
+    ans.celsius = ((float)an.celsius*(9.0/5.0))+32.0;
+    return ans;
 }
 
 
-int main()
+int main(void)
 {
     double num;
-   float inch;
-   float a;
-   int centimeter;
-    cout<<"Enter the doller you want convert into rupees :";
-    cin>>num;
+    float inch;
+    float a;
+    int centimeter;
+    cout << "Enter the doller you want convert into rupees :";
+    cin >> num;
     convert(num);
-    cout<<"Enter the feet you want convert into inch :";
-    cin>>inch;
-    a=convert(inch);
-    cout<<"The inch is :"<<a<<endl;
-    cout<<"Enter the inch you want convert into centimeter :";
-    cin>>centimeter;
+    cout << "Enter the feet you want convert into inch :";
+    cin >> inch;
+    a = convert(inch);
+    cout << "The inch is :" << a << endl;
+    cout << "Enter the inch you want convert into centimeter :";
+    cin >> centimeter;
     convert(centimeter);
+    rom Cel;
+    cout << "Enter the celsius : ";
+    cin >> Cel.celsius;
+    rom ans;
+    ans=to_farenheit(Cel);
+    cout << Cel.celsius << " is equal to  " << ans.celsius << " farnheit" << endl;
 
-    
-
-
-}
+}                                                                                       .
+ 
